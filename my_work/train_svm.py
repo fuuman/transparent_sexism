e941814f713a76d9d2b6a928dc2e9d4e5031e8bc
Marco Schanz <mschanz88@web.de>
2020-09-03 20:34:38 +0200

    build pipeline, add more models

--- my_work/train_svm.py
+++ my_work/train_svm.py
@@ -1,64 +1,61 @@
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from mlxtend.feature_selection import ColumnSelector
 from my_work.unsex_data import UnsexData
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from xgboost.sklearn import XGBClassifier
 import spacy
 import pickle
 
 nlp = spacy.load("en_core_web_sm")
 
 
-def _tokenize(sentence):
-    try:
-        doc = nlp(sentence)
-    except:
-        print(type(sentence))
-        print(sentence)
-    return ' '.join([token.lemma_ for token in doc])
-
-
-if __name__ == '__main__':
+def train(model):
     ud = UnsexData()
 
-    X, y = ud.get_raw_data()
-    X = X.map(_tokenize)
-
-    tf_idf_vec = TfidfVectorizer(max_features=5000)
-    tf_idf_vec.fit(X)
+    X, y = ud.get_preprocessed_data()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-    label_encoder = LabelEncoder()
-    y_train = label_encoder.fit_transform(y_train)
-    y_test = label_encoder.fit_transform(y_test)
+    svm_model = Pipeline(steps=[('col_selector', ColumnSelector(cols=['text'], drop_axis=True)),
+                                ('tfidf', TfidfVectorizer(max_features=5000)),
+                                ('classifier', model)])
+    svm_model.fit(X_train, y_train)
+    y_pred = svm_model.predict(X_test)
 
-    X_train = tf_idf_vec.transform(X_train)
-    X_test = tf_idf_vec.transform(X_test)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    for reg in ['l1', 'l2']:
-        if reg == 'l2':
-            svm = LinearSVC()
-        else:
-            svm = LinearSVC(loss='squared_hinge', penalty=reg, dual=False)
-        svm.fit(X_train, y_train)
-        y_pred = svm.predict(X_test)
 
-        print(f'### SVM ({reg})')
-        print(confusion_matrix(y_test, y_pred))
-        print(classification_report(y_test, y_pred))
+if __name__ == '__main__':
+    print('###### SVM L1 ######')
+    svm_l1 = LinearSVC(loss='squared_hinge', penalty='l1', dual=False)
+    train(svm_l1)
 
-        # save svm model trained on unsex data
-        pickle.dump(svm, open(f'my_svm_{reg}.pkl', 'wb'))
+    print('###### SVM L2 ######')
+    svm_l2 = LinearSVC()
+    train(svm_l2)
 
-    # testing
-    # while 1:
-    #     print('')
-    #     i = input('Text: ')
-    #     pred = svm.predict(tf_idf_vec.transform([_tokenize(i)]))[0]
-    #     print('SEXISTISCH!!' if pred == 1 else 'Brav.')
-    #     print('')
+    print('###### Logistic Regression ######')
+    lr = LogisticRegression()
+    train(lr)
+
+    print('###### XGBoost ######')
+    xgboost = XGBClassifier()
+    train(xgboost)
+
+
+# save svm model trained on unsex data
+# pickle.dump(svm_model, open(f'my_svm_{reg}.pkl', 'wb'))
+
+# testing
+# while 1:
+#     print('')
+#     i = input('Text: ')
+#     pred = svm.predict(tf_idf_vec.transform([_tokenize(i)]))[0]
+#     print('SEXISTISCH!!' if pred == 1 else 'Brav.')
+#     print('')
 
--- my_work/unsex_data.py
+++ my_work/unsex_data.py
@@ -1,17 +1,20 @@
 import os
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 class UnsexData:
-    REPO_DIR = os.path.dirname(os.path.abspath('my_work'))
-    MY_WORK_PATH = os.path.join(REPO_DIR, 'my_work')
-    DATA_PATH = os.path.join(MY_WORK_PATH, 'icwsm2020_data')
-    ALL_DATA_FILE_PATH = os.path.join(DATA_PATH, 'all_data.csv')
-    ALL_DATA_ANNOTATIONS_FILE_PATH = os.path.join(DATA_PATH, 'all_data_annotations.csv')
+    DATA_DIR = os.path.abspath('icwsm2020_data')
+    ALL_DATA_FILE_PATH = os.path.join(DATA_DIR, 'all_data.csv')
+    ALL_DATA_ANNOTATIONS_FILE_PATH = os.path.join(DATA_DIR, 'all_data_annotations.csv')
 
     def __init__(self):
         self.all_data = pd.read_csv(self.ALL_DATA_FILE_PATH, sep='\t')
         # self.all_data_annotations = pd.read_csv(self.ALL_DATA_ANNOTATIONS_FILE_PATH, sep='\t')
 
-    def get_raw_data(self):
-        return self.all_data['text'].dropna(), self.all_data['sexist'].dropna()
+    def get_preprocessed_data(self):
+        X = pd.DataFrame(self.all_data['text'].dropna())
+        y = pd.DataFrame(self.all_data['sexist'].dropna())
+        y['sexist'] = LabelEncoder().fit_transform(y['sexist'])
+        y = y.to_numpy().ravel()
+        return X, y
