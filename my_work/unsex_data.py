153307e82de7bf1cc1d60f2002a0b9ce2ce40be7
Marco Schanz <mschanz88@web.de>
2020-10-05 15:21:12 +0200

    fix unsex data model

--- my_work/unsex_data.py
+++ my_work/unsex_data.py
@@ -4,8 +4,11 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class UnsexData:
-    DATA_DIR = os.path.join('/run', 'media', 'marco', 'Daten', 'GitHub',
-                            'feature-importance', 'my_work', 'icwsm2020_data')
+    if os.name == 'posix':
+        DATA_DIR = os.path.join('/run', 'media', 'marco', 'Daten', 'GitHub',
+                                'feature-importance', 'my_work', 'icwsm2020_data')
+    else:
+        DATA_DIR = os.path.join('D:\\', 'GitHub', 'feature-importance', 'my_work', 'icwsm2020_data')
     ALL_DATA_FILE_PATH = os.path.join(DATA_DIR, 'all_data.csv')
     ALL_DATA_ANNOTATIONS_FILE_PATH = os.path.join(DATA_DIR, 'all_data_annotations.csv')
 
