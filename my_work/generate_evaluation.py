af3dec4c4affcb81d7228c72834ba77bbec914a5
Marco Schanz <mschanz88@web.de>
2020-10-05 15:21:01 +0200

    update explanations

--- my_work/generate_evaluation.py
+++ my_work/generate_evaluation.py
@@ -38,14 +38,14 @@ def _get_top_k_features(features, scores, k):
     return [fe_sc[0] for fe_sc in sorted(comb, key=lambda i: i[1], reverse=True)][:k]
 
 
-def print_explanation(tweet_id, model_name, ex_methods=None, k=3):
+def print_explanation(tweet_id, model_name, ex_methods=None, k=4):
     if ex_methods is None:
         ex_methods = ['lime', 'shap']
-    print(f'###### Explanations ({model_name}) ######')
+    # print(f'###### Explanations ({model_name}) ######')
     for ex_method in ex_methods:
         f, s = _load_features_and_scores(model_name, ex_method)
         print(f'{ex_method.upper()}: {_get_top_k_features(f[tweet_id], s[tweet_id], k)}')
-    print()
+    # print()
 
 
 def print_prediction_and_label(tweet_id, model_name):
@@ -60,9 +60,9 @@ def print_prediction_and_label(tweet_id, model_name):
     tweets, labels = ud.get_preprocessed_data()
     label_idx = labels[tweets.index(tweet)]
     prediction = pipeline.predict([tweet])
-    print(f'###### Classes ######')
+    # print(f'###### Classes ######')
     print(f'Prediction of {model_name.upper()}: {classes[prediction[0]]}')
-    print(f'Real class: {classes[label_idx]}')
+    # print(f'Real class: {classes[label_idx]}')
 
 
 if __name__ == '__main__':
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     tweet_ids = [tweet_id for tweet_id, scores in enumerate(s) if sum(scores) != 0]
     tweets = _load_tweets()
 
-    for i in tweet_ids[:1]:
+    for i in tweet_ids[22:34]:
         print('###### Original Tweet ######')
         print(f'{tweets[i]}\n')
         print_explanation(i, model_name)
