0b21ff75d22449fc421d437bdef7a1101ed2496a
Marco Schanz <mschanz88@web.de>
2020-12-01 15:25:45 +0100

    refactor experiments names

--- _utils/experiments.py
+++ _utils/experiments.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Experiments(Enum):
-    ORIG_AND_ORIG = 0
-    ORIG_MODS_AND_ORIG = 1
-    ORIG_AND_ORIG_MODS = 2
-    ORIG_MODS_AND_ORIG_MODS = 3
+    Train_Orig_Test_Orig = 0
+    Train_Mixed_Test_Orig = 1
+    Train_Orig_Test_Mixed = 2
+    Train_Mixed_Test_Mixed = 3
